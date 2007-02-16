15cd553c50e6d7f5d622683430e9f9e579390a5a
brieb <brieb@66a7d52b-23fa-0310-b706-d52274950f5d>
2007-02-16 01:01:40 +0000

    Log Message for changeset 184 is incorrect. It doe s not implement #73, but instead fixes #76
    git-svn-id: http://www.phpbms.org/svn/trunk@185 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/index.php
+++ phpbms/index.php
@@ -40,7 +40,7 @@
 	require_once("include/common_functions.php");
 	require_once("include/login_include.php");
 	
-	if(!isset($_SESSION["app_path"]))
+	if(!isset($_SESSION["app_path"]));
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
