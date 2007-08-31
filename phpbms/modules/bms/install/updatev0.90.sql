59d80f324a1adbfa9857e55ad1799a84d363fd7f
mipalmer <mipalmer@66a7d52b-23fa-0310-b706-d52274950f5d>
2007-08-31 00:56:35 +0000

    Applying noelmul's patch to increase the state/province for international areas. Closes #175
    git-svn-id: http://www.phpbms.org/svn/trunk@297 66a7d52b-23fa-0310-b706-d52274950f5d

---
+++ phpbms/modules/bms/install/updatev0.90.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `clients` ALTER COLUMN `state` `state` varchar(20) default NULL; 
+ALTER TABLE `clients` ALTER COLUMN `shiptostate` `shiptostate` varchar(20) default NULL; 
+ALTER TABLE `invoices` ALTER COLUMN `state` `state` varchar(20) default NULL; 
--- phpbms/modules/bms/invoices_addedit.php
+++ phpbms/modules/bms/invoices_addedit.php
@@ -237,7 +237,7 @@
 			</p>
 			<p class="cszP">
 				<label for="state">state/prov</label><br />
-				<input name="state" type="text" id="state" value="<?php echo htmlQuotes($therecord["state"])?>" size="3" maxlength="5" tabindex="6" />
+				<input name="state" type="text" id="state" value="<?php echo htmlQuotes($therecord["state"])?>" size="10" maxlength="20" tabindex="6" />
 			</p>
 			<p class="cszP">
 				<label for="postalcode">zip/postal code</label><br />
