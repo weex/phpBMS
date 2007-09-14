f59184fabe99601a8f76f662a378f68871c3c32d
mipalmer <mipalmer@66a7d52b-23fa-0310-b706-d52274950f5d>
2007-09-14 11:50:59 +0000

    Fixes: #183 syntax error in sql script.
    git-svn-id: http://www.phpbms.org/svn/trunk@306 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/modules/bms/install/updatev0.90.sql
+++ phpbms/modules/bms/install/updatev0.90.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `clients` ALTER COLUMN `state` `state` varchar(20) default NULL; 
-ALTER TABLE `clients` ALTER COLUMN `shiptostate` `shiptostate` varchar(20) default NULL; 
-ALTER TABLE `invoices` ALTER COLUMN `state` `state` varchar(20) default NULL; 
+ALTER TABLE `clients` ALTER COLUMN `state` varchar(20) default NULL; 
+ALTER TABLE `clients` ALTER COLUMN `shiptostate` varchar(20) default NULL; 
+ALTER TABLE `invoices` ALTER COLUMN `state` varchar(20) default NULL; 
