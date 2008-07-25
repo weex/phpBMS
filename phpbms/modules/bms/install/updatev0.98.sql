0a81d97c48742a678910b898a553cb2d57556aa4
nate <nate@66a7d52b-23fa-0310-b706-d52274950f5d>
2008-07-25 22:23:29 +0000

    * Updated the bms module's updatev0.98.sql to fixed broken delete button name in tax areas.
    git-svn-id: http://www.phpbms.org/svn/trunk@424 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/modules/bms/install/updatev0.98.sql
+++ phpbms/modules/bms/install/updatev0.98.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `clients` ADD COLUMN `taxid` VARCHAR(64) default NULL AFTER `webaddress`;
 INSERT INTO `settings` (`name`, `value`) VALUES ('company_taxid', '');
+UPDATE `tabledefs` SET `deletebutton` = 'inactivate' WHERE `displayname` = 'Tax Areas';
