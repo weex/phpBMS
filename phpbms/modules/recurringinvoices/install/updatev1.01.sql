df211f2da650b95c3ccb229bd31251be8723da65
brieb <brieb@66a7d52b-23fa-0310-b706-d52274950f5d>
2008-08-01 18:36:41 +0000

    * Fixes #282 - Bad update mysql file for recurring invoices  * removed garbage file
    git-svn-id: http://www.phpbms.org/svn/trunk@425 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/modules/recurringinvoices/install/updatev1.01.sql
+++ phpbms/modules/recurringinvoices/install/updatev1.01.sql
@@ -1,4 +1,5 @@
-INSERT INTO `menu` (`id`, `name`, `link`, `parentid`, `displayorder`, `createdby`, `modifiedby`, `creationdate`, `modifieddate`, `roleid`) VALUES (521, 'Recurring Invoices','search.php?id=400',204,17,1,1,NOW(),NOW(),30);INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('400', 'edit', '1', '0', '0', '0');
-INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('400', 'new', '0', '0', '0', '0');
-INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('400', 'select', '1', '0', '0', '0');
-INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('400', 'printex', '0', '0', '0', '0');
+INSERT INTO `menu` (`id`, `name`, `link`, `parentid`, `displayorder`, `createdby`, `modifiedby`, `creationdate`, `modifieddate`, `roleid`) VALUES (521, 'Recurring Invoices','search.php?id=400',204,17,1,1,NOW(),NOW(),30);
+INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('edit', '1', '0', '0', '0');
+INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('new', '0', '0', '0', '0');
+INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('select', '1', '0', '0', '0');
+INSERT INTO `tableoptions` (`tabledefid`, `name`, `option`, `othercommand`, `roleid`, `displayorder`) VALUES ('printex', '0', '0', '0', '0');
