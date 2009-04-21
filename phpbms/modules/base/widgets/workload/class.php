e9efd15241d9a901f9d747d14a1552d84b044adb
nate <nate@66a7d52b-23fa-0310-b706-d52274950f5d>
2009-04-21 18:01:12 +0000

    * Added license to top of files.
    git-svn-id: http://www.phpbms.org/svn/trunk@505 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/modules/base/widgets/workload/class.php
+++ phpbms/modules/base/widgets/workload/class.php
@@ -1,4 +1,41 @@
 <?php
+/*
+ $Rev: 311 $ | $LastChangedBy: brieb $
+ $LastChangedDate: 2007-10-02 19:51:27 -0600 (Tue, 02 Oct 2007) $
+ +-------------------------------------------------------------------------+
+ | Copyright (c) 2004 - 2007, Kreotek LLC                                  |
+ | All rights reserved.                                                    |
+ +-------------------------------------------------------------------------+
+ |                                                                         |
+ | Redistribution and use in source and binary forms, with or without      |
+ | modification, are permitted provided that the following conditions are  |
+ | met:                                                                    |
+ |                                                                         |
+ | - Redistributions of source code must retain the above copyright        |
+ |   notice, this list of conditions and the following disclaimer.         |
+ |                                                                         |
+ | - Redistributions in binary form must reproduce the above copyright     |
+ |   notice, this list of conditions and the following disclaimer in the   |
+ |   documentation and/or other materials provided with the distribution.  |
+ |                                                                         |
+ | - Neither the name of Kreotek LLC nor the names of its contributore may |
+ |   be used to endorse or promote products derived from this software     |
+ |   without specific prior written permission.                            |
+ |                                                                         |
+ | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS     |
+ | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       |
+ | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A |
+ | PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT      |
+ | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   |
+ | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT        |
+ | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,   |
+ | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY   |
+ | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT     |
+ | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE   |
+ | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.    |
+ |                                                                         |
+ +-------------------------------------------------------------------------+
+*/
     class wdgta1aec114954b37c104747d4e851c728c extends widget{
 
         var $uuid ="wdgt:a1aec114-954b-37c1-0474-7d4e851c728c";
