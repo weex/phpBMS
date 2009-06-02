506321751be69926db0691b7a0355e4ab0d52afa
nate <nate@66a7d52b-23fa-0310-b706-d52274950f5d>
2009-06-02 22:45:50 +0000

    * Reports with global access now correctly displayed.
    git-svn-id: http://www.phpbms.org/svn/trunk@558 66a7d52b-23fa-0310-b706-d52274950f5d

--- phpbms/include/print_class.php
+++ phpbms/include/print_class.php
@@ -66,14 +66,18 @@
 			if(!$queryresult) $error = new appError(500,"Error retreving table info.");
 			$therecord = $this->db->fetchArray($queryresult);
 			$this->maintable = $therecord["maintable"];
-			$this->uuid = $therecord["uuid"];
+			$this->tableuuid = $therecord["uuid"];
 
-			$securitywhere = "";
+			$securitywhere="";
+			if ($_SESSION["userinfo"]["admin"]!=1 && count($_SESSION["userinfo"]["roles"])>0){
 
-			if ($_SESSION["userinfo"]["admin"] != 1 && count($_SESSION["userinfo"]["roles"]) > 0)
-				$securitywhere=" AND roleid IN (".implode(",",$_SESSION["userinfo"]["roles"]).",'')";
+				foreach($_SESSION["userinfo"]["roles"] as $roleUUID)
+					$securitywhere .= ",'".$roleUUID."'";
 
-			$querystatement="
+				$securitywhere=" AND( `roleid` IN (''".$securitywhere.") OR `roleid` IS NULL)";
+			}
+
+			$querystatement = "
 				SELECT
 					`id`,
 					`name`,
@@ -85,15 +89,16 @@
 					`reports`
 				WHERE
 					(
-						tabledefid = ''
+						`tabledefid` = ''
 						OR
-						tabledefid='".$this->uuid."'
+						`tabledefid` = '".$this->tableuuid."'
 					) ".$securitywhere."
 				ORDER BY
 					`tabledefid` DESC,
 					`displayorder` DESC,
 					`name`
 				";
+
 			$queryresult = $this->db->query($querystatement);
 			if(!$queryresult) $error = new appError(500,"Error retreving reports.");
 			$this->reports = $queryresult;
@@ -112,15 +117,42 @@
 		function getSaved($userid,$type){
 
 			$securitywhere="";
-			if ($_SESSION["userinfo"]["admin"]!=1 && count($_SESSION["userinfo"]["roles"])>0)
-				$securitywhere=" AND roleid IN (".implode(",",$_SESSION["userinfo"]["roles"]).",'')";
-			$querystring="SELECT id,name,userid FROM usersearches WHERE tabledefid=".$this->tableid." and type=\"".$type."\" and((userid=0 ".$securitywhere.") or userid=\"".$userid."\") order by userid,name";
+			if ($_SESSION["userinfo"]["admin"]!=1 && count($_SESSION["userinfo"]["roles"])>0){
+
+				foreach($_SESSION["userinfo"]["roles"] as $roleUUID)
+					$securitywhere .= ",'".$roleUUID."'";
+
+				$securitywhere = substr($securitywhere, 1);
+
+				$securitywhere=" AND roleid IN (".$securitywhere.")";
+			}
+			$querystring = "
+				SELECT
+					`id`,
+					`name`,
+					`userid`
+				FROM
+					`usersearches`
+				WHERE
+					`tabledefid`='".$this->tableuuid."'
+					AND
+					type=\"".$type."\"
+					AND
+					(
+						(userid = '0' ".$securitywhere.")
+						OR
+						userid=\"".$userid."\"
+					)
+				ORDER BY
+					`userid`,
+					`name`";
+
 			$thequery = $this->db->query($querystring);
 			return $thequery;
 		}//end function
 
 
-		function  donePrinting($backurl){
+		function donePrinting($backurl){
 			if(!$backurl)
 				goURL("search.php?id=".$this->tableid);
 			else
